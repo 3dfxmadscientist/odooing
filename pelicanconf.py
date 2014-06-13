a3b206cbb7920cba832014e80fdb75c43e98e22c
Daniel Reis <dreis.pt@gmail.com>
2014-06-13 14:51:41 +0100

    First post

---
+++ pelicanconf.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Daniel Reis'
+SITENAME = u'Odooing'
+SITEURL = ''
+
+TIMEZONE = 'Europe/Lisbon'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+#LINKS = (('Odoo', 'http://odoo.com/'),
+#         ('Odoo Community Association', 'http://odoo-community-association.org/'),)
+
+# Social widget
+SOCIAL = (('Twitter', 'https://twitter.com/reis_pt'),
+          ('Github', 'https://github.com/dreispt'),
+          ('Launchpad', 'https://launchpad.net/~dreis-pt'),
+          ('LinkedIn', 'https://www.linkedin.com/in/danielgreis'),)
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
+
+THEME = 'notmyidea'
